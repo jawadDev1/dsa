b48175a05f210bc06e81ee275500bea318066e71
Jawad Ali <149782239+jawadDev1@users.noreply.github.com>
2025-04-16 21:33:01 +0500

    feat: String -> Anagram

---
+++ data_structures/Strings/index.js
@@ -0,0 +1,35 @@
+function log(value) {
+  console.log("Value ====> ", value);
+}
+
+const str = "There goes the testing string";
+
+// Anagram String
+
+function isAnagram(str1, str2) {
+  if (str1.length !== str2.length) return false;
+  const result = {};
+  for (let i = 0; i < str1.length; i++) {
+    result[str1[i]] = (result[str1[i]] || 0) + 1;
+  }
+
+  for (let i = 0; i < str2.length; i++) {
+    if(!result[str2[i]]) return false;
+    result[str2[i]]--;
+  }
+
+  return true;
+
+}
+
+const str1 = "abs";
+const str2 = "silent";
+
+log(isAnagram(str1, str2));
+
+// const obj = {
+//     'a' : 1
+// }
+const obj = [];
+
+log(obj[0])
